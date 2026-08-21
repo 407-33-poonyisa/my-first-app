db280afc41a86cb0b99b782aa3a9c7f514a1880e
407-33-poonyisa <54747@yupparaj.ac.th>
2026-08-21 11:39:24 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -54,14 +54,14 @@ def show_result_dialog(ans1, ans2):
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
     # ตรวจข้อ 3
-    if u_ans3 == "fish":
+    if u_ans3 == "book":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 
     # ตรวจข้อ 4
-    if u_ans4 == "fish":
+    if u_ans4 == "cherry":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
